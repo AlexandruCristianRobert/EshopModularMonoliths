fcf753b282e1c804a4c2a20217fdd2fc4ec6bc16
Alexandru Cristian Robert <alexandru.cristian.robert@gmail.com>
2025-03-05 17:21:18 +0200

    db , interceptors for catalog

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,23 +1,36 @@
-﻿namespace Catalog
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Shared.Data.Interceptors;
+
+namespace Catalog
 {
     public static class CatalogModule
     {
         public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
         {
-            //services.AddApplicationServices()
-            //    .AddInfrastructureServices(configuration)
-            //    .AddApiServices(configuration);
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
 
+
+            var connectionString = configuration.GetConnectionString("Database");
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<CatalogDbContext>((sp,options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseNpgsql(connectionString);
+            });
+
+            services.AddScoped<IDataSeeder, CatalogDataSeeder>();
 
             return services;
         }
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
+            app.UseMigration<CatalogDbContext>();
 
-            //app
-            //    .UseApplicationServices()
-            //    .useInfrastructureServices()
-            //    .UseApiServices();
             return app;
         }
     }
