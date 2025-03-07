86caf561d49167e2ad64b0127c765d122edb40c5
Cristian Alexandru <cristian.alexandru@spark-radiance.com>
2025-03-07 13:32:38 +0200

    cqrss for catalog

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Catalog.Products.Features.GetProductByCategory
+{
+    public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+    public record GetProductsByCategoryResult(IEnumerable<ProductDto> Products);
+    internal class GetProductsByCategoryQueryHandler(CatalogDbContext dbContext) : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+    {
+        public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            var products = await dbContext.Products
+                .Where(p => p.Category.Contains(query.Category))
+                .AsNoTracking()
+                .OrderBy(p => p.Name)
+                .ToListAsync(cancellationToken);
+
+            var productDtos = products.Adapt<List<ProductDto>>();
+
+            return new GetProductsByCategoryResult(productDtos);
+        }
+    }
+}
---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Catalog.Products.Features.GetProductById
+{
+    public record GetProductByIdQuery(Guid ProductId) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(ProductDto Product);
+    internal class GetProductByIdQueryHandler(CatalogDbContext dbContext) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            var product = await dbContext.Products
+                .AsNoTracking()
+                .SingleOrDefaultAsync(p => p.Id == query.ProductId, cancellationToken: cancellationToken);
+
+            if (product == null)
+            {
+                throw new Exception($"Product not found: {query.ProductId}");  
+            }
+
+            var productDto = product.Adapt<ProductDto>();
+
+            return new GetProductByIdResult(productDto);
+        }
+    }
+}
